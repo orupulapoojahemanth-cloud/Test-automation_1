bbf3334d6a4459995e90c873cbe041768d95cf7b
orupulapujahemanth <orupulapoojahemanth@gmail.com>
2026-07-08 12:59:46 +0530

    Fixed Playwright test failure

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,7 +13,7 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
-  retries: 1;
+  retries: 1,
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
